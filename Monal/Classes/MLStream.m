b8de6be35c4e317b758ae82424f59767b77ec0eb
Thilo Molitor <thilo@monal-im.org>
2024-11-02 09:11:40 +0100

    Fix bug in MLStream on slow connections

--- Monal/Classes/MLStream.m
+++ Monal/Classes/MLStream.m
@@ -50,7 +50,7 @@
     //(mutexes can not be unlocked in a thread different from the one it got locked in and NSLock internally uses mutext --> both can not be used)
     dispatch_semaphore_t _read_sem;
 }
-@property (atomic, readonly) void (^incoming_data_handler)(NSData* _Nullable, BOOL, NSError* _Nullable, BOOL allow_next_read);
+@property (atomic, readonly) void (^incoming_data_handler)(NSData* _Nullable, BOOL, NSError* _Nullable);
 @end
 
 @interface MLOutputStream()
@@ -89,7 +89,7 @@
     //this handler will be called by the schedule_read method
     //since the framer swallows all data, nw_connection_receive() and the framer cannot race against each other and deliver reordered data
     weakify(self);
-    _incoming_data_handler = ^(NSData* _Nullable content, BOOL is_complete, NSError* _Nullable st_error, BOOL allow_next_read) {
+    _incoming_data_handler = ^(NSData* _Nullable content, BOOL is_complete, NSError* _Nullable st_error) {
         strongify(self);
         if(self == nil)
             return;
@@ -142,7 +142,7 @@
             [self generateEvent:NSStreamEventEndEncountered];
         
         //try to read again
-        if(!is_complete && !generate_bytes_available_event && allow_next_read)
+        if(!is_complete && !generate_bytes_available_event)
             [self schedule_read];
     };
     return self;
@@ -235,8 +235,7 @@
                 DDLogDebug(@"now calling nw_framer_parse_input inside framer queue");
                 nw_framer_parse_input(self.shared_state.framer, 1, BUFFER_SIZE, nil, ^size_t(uint8_t* buffer, size_t buffer_length, bool is_complete) {
                     DDLogDebug(@"nw_framer_parse_input got callback with is_complete:%@, length=%zu", bool2str(is_complete), (unsigned long)buffer_length);
-                    //we only want to allow new calls to schedule_read if we received some data --> set last arg accordingly
-                    self.incoming_data_handler([NSData dataWithBytes:buffer length:buffer_length], is_complete, nil, buffer_length > 0);
+                    self.incoming_data_handler([NSData dataWithBytes:buffer length:buffer_length], is_complete, nil);
                     return buffer_length;
                 });
             });
@@ -249,8 +248,7 @@
                 NSError* st_error = nil;
                 if(receive_error)
                     st_error = (NSError*)CFBridgingRelease(nw_error_copy_cf_error(receive_error));
-                //we always want to allow new calls to schedule_read --> set last arg to YES
-                self.incoming_data_handler((NSData*)content, is_complete, st_error, YES);
+                self.incoming_data_handler((NSData*)content, is_complete, st_error);
             });
         }
     }
